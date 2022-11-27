c104f0d088ce20a2581818bb8c14dfecc65b3d0c
SuurkaevKarl <suurkarl@gmail.com>
2022-11-27 18:12:02 +0200

    Parandus KMI arvutamise valemis.

--- js/calc.js
+++ js/calc.js
@@ -6,11 +6,11 @@ function calcKMI() {
         if (weight <= 0 || height <= 0) {
             throw 'Invalid input!';
         }
-        result = weight / Math.pow(height, 2);
+        result = weight / Math.pow(height/100, 2);
         if (result <= 0.0) {
             throw 'Invalid result!';
         } else if (result >= 18.5 && result <= 30.0) {
-            alert("Teie kehamassiindeks on " + result.toString + " mis on normide piires.");
+            alert("Teie kehamassiindeks on " + Math.round(result * 100) / 100 + " mis on normide piires.");
         } else if (result < 18.5) {
             console.log(location.hostname + "/under.html?")
             window.location.assign("/under.html");
